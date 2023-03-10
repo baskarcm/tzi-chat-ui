b0417b14a96aef4f106076af7671280d477a68af
baskar <baskarmca.php@gmail.com>
2023-03-10 12:19:42 +0530

    1.2.6

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -128,5 +128,37 @@
     </main>
 </wrapper>
 @include('messenger::scripts')
+<script>
+    function getCaret(el) { 
+    if (el.selectionStart) { 
+        return el.selectionStart; 
+    } else if (document.selection) { 
+        el.focus();
+        var r = document.selection.createRange(); 
+        if (r == null) { 
+            return 0;
+        }
+        var re = el.createTextRange(), rc = re.duplicate();
+        re.moveToBookmark(r.getBookmark());
+        rc.setEndPoint('EndToStart', re);
+        return rc.text.length;
+    }  
+    return 0; 
+}
+$(document).on('keyup','textarea',function (event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        var content = this.value;  
+        var caret = getCaret(this);          
+        if(event.shiftKey){
+            this.value = content.substring(0, caret - 1) + "\n" + content.substring(caret, content.length);
+            event.stopPropagation();
+        } else {
+            this.value = content.substring(0, caret - 1) + content.substring(caret, content.length);
+            $('form').submit();
+        }
+    }
+});
+</script>
 </body>
 </html>
